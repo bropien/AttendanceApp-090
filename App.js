bf8eecab5dfbb435400dfa05d6d0c593f1900f86
bropien <mzakkyraihan@gmail>
2026-03-12 08:29:39 +0700

    P1 prg6 090

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <View style={styles.topcontainer}>
+        <View style={styles.redbox} />
+        <View style={styles.bluebox} />
+        <View style={styles.greenbox} />
+        <View style={styles.orangebox} />
+      </View>
+
+      <View style={styles.bottomcontainer} />
+
     </View>
   );
 }
@@ -14,7 +23,51 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+
+  topcontainer: {
+    height: 200,
+    backgroundColor: '#e6f0f0',
+  },
+
+  redbox: {
+    position: 'absolute',
+    width: 50,
+    height: 30,
+    backgroundColor: 'red',
+    top: 0,
+    left: 0,
+  },
+
+  bluebox: {
+    position: 'absolute',
+    width: 150,
+    height: 60,
+    backgroundColor: 'blue',
+    top: 0,
+    left: 50,
+  },
+
+  greenbox: {
+    position: 'absolute',
+    width: 100,
+    height: 80,
+    backgroundColor: 'green',
+    top: 0,
+    left: 200,
+  },
+
+  orangebox: {
+    position: 'absolute',
+    width: 100,
+    height: 150,
+    backgroundColor: 'orange',
+    top: 0,
+    right: 0,
+  },
+
+  bottomcontainer: {
+    flex: 1,
+    backgroundColor: 'grey',
   },
 });
